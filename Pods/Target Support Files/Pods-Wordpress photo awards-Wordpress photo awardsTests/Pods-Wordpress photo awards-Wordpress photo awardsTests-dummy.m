ab0a6714b5c5cc794b5e01b889fd9958ab187fee
MohamedDiaa <mohameddiaa1988@hotmail.com>
2016-09-01 21:16:33 +0200

    Commit all files

---
+++ Pods/Target Support Files/Pods-Wordpress photo awards-Wordpress photo awardsTests/Pods-Wordpress photo awards-Wordpress photo awardsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wordpress_photo_awards_Wordpress_photo_awardsTests : NSObject
+@end
+@implementation PodsDummy_Pods_Wordpress_photo_awards_Wordpress_photo_awardsTests
+@end
