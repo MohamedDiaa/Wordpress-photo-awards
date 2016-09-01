bd5efd71004bca1e757e43da9ea9665a51667e89
MohamedDiaa <mohameddiaa1988@hotmail.com>
2016-09-01 20:48:05 +0200

    Progress on UI controllers  tableview and cell design inspired from tumblr

--- Wordpress photo awards/MainViewController.swift
+++ Wordpress photo awards/MainViewController.swift
@@ -8,12 +8,18 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet var photosTableView:UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let nib = UINib(nibName: "PhotoTableViewCell", bundle: nil)
+        self.photosTableView.registerNib(nib, forCellReuseIdentifier: "PhotoTableViewCell")
         // Do any additional setup after loading the view.
+        self.photosTableView.estimatedRowHeight = 85.0
+        self.photosTableView.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +28,32 @@ class MainViewController: UIViewController {
     }
     
 
+     //UITableViewDelegate,UITableViewDataSource
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell  = tableView.dequeueReusableCellWithIdentifier("PhotoTableViewCell") as? PhotoTableViewCell
+        
+        return cell ?? UITableViewCell()
+    }
+   
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if let vw = NSBundle.mainBundle().loadNibNamed("PhotoTableViewHeader", owner: nil, options: nil)[0] as? UIView{
+        
+            return vw
+        }
+        return nil
+    }
+ 
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
 }
--- Wordpress photo awards/ViewController.swift
+++ Wordpress photo awards/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let mvc = MainViewController(nibName: "MainViewController", bundle: nil)
+        
+        mvc.view.translatesAutoresizingMaskIntoConstraints = false
+        self.addChildViewController(mvc)
+        let c1 = NSLayoutConstraint(item: self.view, attribute: .Top, relatedBy: .Equal, toItem: mvc.view, attribute: .Top, multiplier: 1.0, constant: 0.0)
+        let c2 = NSLayoutConstraint(item: self.view, attribute: .Bottom, relatedBy: .Equal, toItem: mvc.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        let c3 = NSLayoutConstraint(item: self.view, attribute: .Leading, relatedBy: .Equal, toItem: mvc.view, attribute: .Leading, multiplier: 1.0, constant: 0.0)
+        let c4 = NSLayoutConstraint(item: self.view, attribute: .Trailing, relatedBy: .Equal, toItem: mvc.view, attribute: .Trailing, multiplier: 1.0, constant: 0.0)
+        
+        self.view.addSubview(mvc.view)
+        self.view.addConstraints([c1,c2,c3,c4])
+        self.view.setNeedsLayout()
+        self.view.layoutIfNeeded()
     }
 
     override func didReceiveMemoryWarning() {
